87558ad8022aadd1c419dde6577774de6459ab76
Khaled <kh.almelhem@gmail.com>
2024-05-29 11:54:37 +0200

    first commit

---
+++ src/page/FAQ/FAQ.jsx
@@ -0,0 +1,12 @@
+import FAQSection from "../../components/FAQSection";
+
+const FAQ = () => {
+  return (
+    <div>
+      <h1>FAQ</h1>
+      <FAQSection />
+    </div>
+  );
+};
+
+export default FAQ;
