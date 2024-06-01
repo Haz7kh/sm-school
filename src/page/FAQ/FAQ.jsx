d5f2fa03d6dfc23cdf1a1610d46dc45551430efd
Khaled <kh.almelhem@gmail.com>
2024-06-01 08:56:31 +0200

    update

--- src/page/FAQ/FAQ.jsx
+++ src/page/FAQ/FAQ.jsx
@@ -3,10 +3,10 @@ import FAQSection from "../../components/FAQSection";
 const FAQ = () => {
   return (
     <div>
-      <h1>FAQ</h1>
       <FAQSection />
     </div>
   );
 };
 
+// eslint-disable-next-line react-refresh/only-export-components
 export default FAQ;
